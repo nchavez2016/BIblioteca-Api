e2a52b24612ce74ac9a29012332d3eb31d937793
Napoleon <napo_27@hotmail.com>
2026-09-11 17:16:55 -0500

    Update AutoresController.cs

--- BibliotecaAPI/Controllers/V1/AutoresController.cs
+++ BibliotecaAPI/Controllers/V1/AutoresController.cs
@@ -75,8 +75,7 @@ namespace BibliotecaAPI.Controllers.V1
             //logger.LogWarning("Obteniendo listado de autore");
             //logger.LogError("Obteniendo listado de autore");
             //logger.LogCritical("Obteniendo listado de autore");
-            //throw new NotImplementedException();--codigo para prueba de error
-            throw new ApplicationException("Linea que se debe borrar");
+            //throw new NotImplementedException();--codigo para prueba de error            
             return await servicioAutoresV1.Get(paginacionDTO);
 
         }
